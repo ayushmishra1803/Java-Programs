022c8b5b78fc826f88b000946ebda88bbe96afa8
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-05-09 22:19:48 +0530

    new code

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,21 @@
+// checking Palindrome
+
+import java.util.*;
+
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String s = sc.next();
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            r = r + s.charAt(i);
+        }
+
+        if (s.equals(r)) {
+            System.out.println("It is a palindrome");
+        } else {
+            System.out.println("It is not a palindrome");
+        }
+    }
+}
